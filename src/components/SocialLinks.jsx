23a5764479e7b42849ec65e69101bc6c3271cf35
Bhanutejadomala <bhanutejadomala@gmail.com>
2024-04-03 20:20:44 +0530

    add resume

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -41,7 +41,7 @@ const SocialLinks = () => {
         </>
       ),
       style: "rounded-br-md",
-      href: "/resume.pdf",
+      href: "./resume/BhanuTejaDomala.pdf",
       download: true,
     },
   ];
